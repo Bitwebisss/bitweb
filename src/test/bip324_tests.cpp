7a922fb8f8f96c6e90f7c3e4e6dddbbbb60d53dc
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-03-25 22:48:27 +0200

    Update bip324_tests.cpp

--- src/test/bip324_tests.cpp
+++ src/test/bip324_tests.cpp
@@ -170,29 +170,29 @@ BOOST_AUTO_TEST_CASE(packet_test_vectors) {
 
     // The test vectors are converted using the following Python code in the BIP bip-0324/ directory:
     //
-    // import sys
-    // import csv
-    // with open('packet_encoding_test_vectors.csv', newline='', encoding='utf-8') as csvfile:
-    //     reader = csv.DictReader(csvfile)
-    //     quote = lambda x: "\"" + x + "\""
-    //     for row in reader:
-    //         args = [
-    //             row['in_idx'],
-    //             quote(row['in_priv_ours']),
-    //             quote(row['in_ellswift_ours']),
-    //             quote(row['in_ellswift_theirs']),
-    //             "true" if int(row['in_initiating']) else "false",
-    //             quote(row['in_contents']),
-    //             row['in_multiply'],
-    //             quote(row['in_aad']),
-    //             "true" if int(row['in_ignore']) else "false",
-    //             quote(row['mid_send_garbage_terminator']),
-    //             quote(row['mid_recv_garbage_terminator']),
-    //             quote(row['out_session_id']),
-    //             quote(row['out_ciphertext']),
-    //             quote(row['out_ciphertext_endswith'])
-    //         ]
-    //         print("    TestBIP324PacketVector(\n        " + ",\n        ".join(args) + ");")
+     import sys
+     import csv
+     with open('packet_encoding_test_vectors.csv', newline='', encoding='utf-8') as csvfile:
+         reader = csv.DictReader(csvfile)
+         quote = lambda x: "\"" + x + "\""
+         for row in reader:
+             args = [
+                 row['in_idx'],
+                 quote(row['in_priv_ours']),
+                 quote(row['in_ellswift_ours']),
+                 quote(row['in_ellswift_theirs']),
+                 "true" if int(row['in_initiating']) else "false",
+                 quote(row['in_contents']),
+                 row['in_multiply'],
+                 quote(row['in_aad']),
+                 "true" if int(row['in_ignore']) else "false",
+                 quote(row['mid_send_garbage_terminator']),
+                 quote(row['mid_recv_garbage_terminator']),
+                 quote(row['out_session_id']),
+                 quote(row['out_ciphertext']),
+                 quote(row['out_ciphertext_endswith'])
+             ]
+             print("    TestBIP324PacketVector(\n        " + ",\n        ".join(args) + ");")
     TestBIP324PacketVector(
         1,
         "61062ea5071d800bbfd59e2e8b53d47d194b095ae5a4df04936b49772ef0d4d7",
