453cc44944bb2d32355d79483a0c411bef1173b8
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-06 23:31:33 +0300

    Update validation_tests.cpp

--- src/test/validation_tests.cpp
+++ src/test/validation_tests.cpp
@@ -145,7 +145,7 @@ BOOST_AUTO_TEST_CASE(test_assumeutxo)
     BOOST_CHECK_EQUAL(out110.hash_serialized.ToString(), "b952555c8ab81fec46f3d4253b7af256d766ceb39fb7752b9d18cdf4a0141327");
     BOOST_CHECK_EQUAL(out110.m_chain_tx_count, 111U);
 
-    const auto out110_2 = *params->AssumeutxoForBlockhash(uint256{"6affe030b7965ab538f820a56ef56c8149b7dc1d1c144af57113be080db7c397"});
+    const auto out110_2 = *params->AssumeutxoForBlockhash(uint256{"7d16cb9e8f57a486e33741129c3ac01f54f466c371bcbf953a2ee521aecaeb9d"});
     BOOST_CHECK_EQUAL(out110_2.hash_serialized.ToString(), "b952555c8ab81fec46f3d4253b7af256d766ceb39fb7752b9d18cdf4a0141327");
     BOOST_CHECK_EQUAL(out110_2.m_chain_tx_count, 111U);
 }
