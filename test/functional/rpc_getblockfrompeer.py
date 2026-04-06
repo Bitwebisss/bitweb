ef6b4dc37dd46d3455867648a95e7ba4af7d094f
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-07 02:57:27 +0300

    Update rpc_getblockfrompeer.py

--- test/functional/rpc_getblockfrompeer.py
+++ test/functional/rpc_getblockfrompeer.py
@@ -146,7 +146,7 @@ class GetBlockFromPeerTest(BitcoinTestFramework):
         self.sync_blocks([self.nodes[0], pruned_node])
         pruneheight += 251
         assert_equal(pruned_node.pruneblockchain(700), pruneheight)
-        assert_equal(pruned_node.getblock(pruned_block)["hash"], "196ee3a1a6db2353965081c48ef8e6b031cb2115d084bec6fec937e91a2c6277")
+        assert_equal(pruned_node.getblock(pruned_block)["hash"], "5e83bcb8d14a2194067fb7df8ae86770f2349ce44310bf50849c9141485b69cc")
 
         self.log.info("Fetched block can be pruned again when prune height exceeds the height of the tip at the time when the block was fetched")
         self.generate(self.nodes[0], 250, sync_fun=self.no_op)
