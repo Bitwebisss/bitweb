ca3eda4acbdeffff9b940c7d9fccdc2607e2026d
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-08 09:26:39 +0300

    Update chainparams.cpp

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -93,12 +93,12 @@ public:
         consensus.script_flag_exceptions.emplace( // Taproot exception
             uint256{"0000000000000000000f14c35b2d841e986ab5441de8c585d5ffe55ea1e395ad"}, SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_WITNESS);
         */
-        consensus.BIP34Height = 50;
+        consensus.BIP34Height = 51;
         consensus.BIP34Hash = uint256{};
         consensus.BIP65Height = 2;
         consensus.BIP66Height = 2;
         consensus.CSVHeight = 500;
-        consensus.SegwitHeight = 500;
+        consensus.SegwitHeight = 2;
         consensus.MinBIP9WarningHeight = 4034; // segwit activation height + miner confirmation window
         consensus.powLimit = uint256{"00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff"};
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
