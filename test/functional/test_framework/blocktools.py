52ed930c4d1c64b11666671c8c74cb0c154718f1
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-09 13:36:06 +0300

    Update blocktools.py

--- test/functional/test_framework/blocktools.py
+++ test/functional/test_framework/blocktools.py
@@ -67,7 +67,7 @@ NORMAL_GBT_REQUEST_PARAMS = {"rules": ["segwit"]}
 VERSIONBITS_LAST_OLD_BLOCK_VERSION = 4
 MIN_BLOCKS_TO_KEEP = 288
 
-REGTEST_RETARGET_PERIOD = 1
+REGTEST_HALVING_PERIOD = 150
 
 REGTEST_N_BITS = 0x207fffff  # difficulty retargeting is disabled in REGTEST chainparams"
 REGTEST_TARGET = 0x7fffff0000000000000000000000000000000000000000000000000000000000
@@ -144,7 +144,7 @@ def script_BIP34_coinbase_height(height):
     return CScript([CScriptNum(height)])
 
 
-def create_coinbase(height, pubkey=None, *, script_pubkey=None, extra_output_script=None, fees=0, nValue=50, halving_period=REGTEST_RETARGET_PERIOD):
+def create_coinbase(height, pubkey=None, *, script_pubkey=None, extra_output_script=None, fees=0, nValue=50, halving_period=REGTEST_HALVING_PERIOD):
     """Create a coinbase transaction.
 
     If pubkey is passed in, the coinbase output will be a P2PK output;
