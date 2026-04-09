1edf3e99bf2df7979aee9ea164f50f6c8e69e205
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-09 13:55:20 +0300

    Update client.cpp

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -65,7 +65,6 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "getbalance", 3, "avoid_reuse" },
     { "getblockfrompeer", 1, "peer_id" },
     { "getblockhash", 0, "height" },
-/*    { "getargon2idpowblockhash", 0, "height" },  Bitweb Params */
     { "waitforblockheight", 0, "height" },
     { "waitforblockheight", 1, "timeout" },
     { "waitforblock", 1, "timeout" },
