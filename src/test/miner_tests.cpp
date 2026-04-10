1af26cf1647d9989cc7d6b5e1233f9aacc583851
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-11 00:05:17 +0300

    Update miner_tests.cpp

--- src/test/miner_tests.cpp
+++ src/test/miner_tests.cpp
@@ -756,9 +756,9 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
             block.nVersion = VERSIONBITS_TOP_BITS;
             block.nTime = Assert(m_node.chainman)->ActiveChain().Tip()->GetMedianTimePast()+1;
             txCoinbase.version = 1;
-            txCoinbase.vin[0].scriptSig = CScript{} << (current_height + 1) << bi.extranonce;
+            txCoinbase.vin[0].scriptSig = CScript{} << CScriptNum(current_height + 1) << CScriptNum(bi.extranonce);
             txCoinbase.vout.resize(1); // Ignore the (optional) segwit commitment added by CreateNewBlock (as the hardcoded nonces don't account for this)
-            txCoinbase.vout[0].scriptPubKey = CScript() << OP_TRUE;
+            txCoinbase.vout[0].scriptPubKey = CScript();
             block.vtx[0] = MakeTransactionRef(txCoinbase);
             if (txFirst.size() == 0)
                 baseheight = current_height;
