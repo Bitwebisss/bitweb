49065eb44c229d2eff54c7c98c73afad577b8dc7
Bitwebisss <196428942+Bitwebisss@users.noreply.github.com>
2026-04-10 14:34:17 +0300

    Update feature_assumevalid.py

--- test/functional/feature_assumevalid.py
+++ test/functional/feature_assumevalid.py
@@ -157,7 +157,7 @@ class AssumeValidTest(BitcoinTestFramework):
             # Send all blocks to node1. All blocks will be accepted.
             for i in range(2202):
                 p2p1.send_without_ping(msg_block(self.blocks[i]))
-                print(msg_block(self.blocks[i])
+                print(msg_block(self.blocks[i]))
             # Syncing 2200 blocks can take a while on slow systems. Give it plenty of time to sync.
             p2p1.sync_with_ping(timeout=960)
         assert_equal(self.nodes[1].getblock(self.nodes[1].getbestblockhash())['height'], 2202)
